a13c126defcf45f9d49f8f375fcc7304a01ca666
Nurlan Purkhanov <ppurkhanov@gmail.com>
2026-03-16 12:16:38 +0500

    created quicksort in python

---
+++ python/quicksort.py
@@ -0,0 +1,14 @@
+# quick sort
+
+def quicksort(array: list) -> list:
+    if len(array) < 2:
+        return array
+    
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([10, 5, 2, 3]))
